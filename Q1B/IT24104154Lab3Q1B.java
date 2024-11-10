b5177364adfe7d56d6fda7d4a09abb964039e553
IT24104154 <IT24104154@my.sliit.lk>
2024-11-10 13:41:29 +0530

    Add files via upload

---
+++ Q1B/IT24104154Lab3Q1B.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IT24104154Lab3Q1B
+{
+	public static void main(String[]args)
+	{
+		double kilo;
+		double price;
+		double x;
+
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the price of 1kg of rice :"); 
+		price=input.nextDouble();
+
+		System.out.println("Enter the number of kilograms you want to buy :");
+		kilo=input.nextDouble();
+		
+		x=kilo*price;
+		System.out.println("total amount is:" +x);
+
+		double dis;
+		dis=x*0.1;
+		System.out.println("Discount:"+dis);
+	}
+}
+	
